61896af1d2d6b937d6898267b8eab38562ed70fe
nexryai <nexryai@sda1.net>
2025-02-21 09:21:08 +0900

    backend: cleanup

--- packages/backend/src/server/api/common/signin.ts
+++ packages/backend/src/server/api/common/signin.ts
@@ -1,31 +1,16 @@
 import Koa from "koa";
 
-import config from "@/config/index.js";
 import { ILocalUser } from "@/models/entities/user.js";
 import { Signins } from "@/models/index.js";
 import { genId } from "@/misc/gen-id.js";
 import { publishMainStream } from "@/services/stream.js";
 
-export default function(ctx: Koa.Context, user: ILocalUser, redirect = false) {
-    if (redirect) {
-        //#region Cookie
-        ctx.cookies.set("igi", user.token!, {
-            path: "/",
-            // SEE: https://github.com/koajs/koa/issues/974
-            // When using a SSL proxy it should be configured to add the "X-Forwarded-Proto: https" header
-            secure: config.url.startsWith("https"),
-            httpOnly: false,
-        });
-        //#endregion
-
-        ctx.redirect(config.url);
-    } else {
-        ctx.body = {
-            id: user.id,
-            i: user.token,
-        };
-        ctx.status = 200;
-    }
+export default function(ctx: Koa.Context, user: ILocalUser) {
+    ctx.body = {
+        id: user.id,
+        i: user.token,
+    };
+    ctx.status = 200;
 
     (async () => {
         // Append signin history
@@ -34,6 +19,7 @@ export default function(ctx: Koa.Context, user: ILocalUser, redirect = false) {
             createdAt: new Date(),
             userId: user.id,
             ip: ctx.ip,
+            // @ts-ignore
             headers: ctx.headers,
             success: true,
         }).then(x => Signins.findOneByOrFail(x.identifiers[0]));
